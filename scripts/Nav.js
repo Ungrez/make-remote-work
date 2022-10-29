acc5c292def0cff807a53cb82631bc1f6a20b234
Piotr Machera <piotr.mache@gmail.com>
2022-10-29 14:50:40 +0200

    nav events and styles

--- scripts/Nav.js
+++ scripts/Nav.js
@@ -3,10 +3,18 @@ class Nav {
     this.openButton = document.querySelector("#nav_menu");
     this.closeButton = document.querySelector(".nav_menu_links_close-button");
     this.menu = document.querySelector("#nav_menu_links");
+    this.dropdown = document.querySelectorAll("li.dropdown");
+    this.dropdownContainer = document.querySelectorAll("ul.dropdown-ul");
+    this.dropdownArrows = document.querySelectorAll("img.dropdown");
   }
   init() {
     this.openButton.addEventListener("click", () => this.openMenu());
     this.closeButton.addEventListener("click", () => this.closeMenu());
+    this.dropdown.forEach((dropdown) =>
+      dropdown.addEventListener("click", (e) =>
+        this.showDropdown(e.target.classList)
+      )
+    );
   }
   closeMenu() {
     this.menu.classList.add("hidden");
@@ -15,6 +23,14 @@ class Nav {
   openMenu() {
     this.menu.classList.remove("hidden");
     document.body.classList.add("hidden");
+  }
+  showDropdown(e) {
+    this.dropdownContainer.forEach((item) => {
+      if (item.id === e[1]) {
+        item.classList.toggle("loaded");
+        e.toggle("loaded");
+      }
+    });
   }
 }
 export const nav = new Nav();
